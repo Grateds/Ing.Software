f38d674246aa856985e5c0301cf3636debee18ee
Abuzaid Karim <karim@Karim.(none)>
2013-10-06 17:34:11 -0300

    Project Structure

---
+++ src/main/java/com/Grateds/Reversi/App.java
@@ -0,0 +1,10 @@
+package com.Grateds.Reversi;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "It's OK" );
+    }
+}
---
+++ src/test/java/com/Grateds/Reversi/CONTROLLER/ControllerTest.java
@@ -0,0 +1,3 @@
+package com.Grateds.Reversi.CONTROLLER;
+
+
