53f69bc30a16404d882d65b5d29444574d7171c0
dastorga <astorgadm@gmail.com>
2013-10-07 23:29:39 -0300

    Class Cell: methods

--- src/main/java/com/Grateds/Reversi/MODEL/Cell.java
+++ src/main/java/com/Grateds/Reversi/MODEL/Cell.java
@@ -1,1 +1,24 @@
 package com.Grateds.Reversi.MODEL;
+
+public class Cell implements Model {
+	
+	private int value; // value of the cell
+	private boolean settable; // field that indicates if cell is settable
+
+	public void initialization() {
+		value = 0;
+		settable = true; // default
+	} // end initialization
+	
+	public void set_value(int new_value) {
+		value = new_value;
+	} // end set_value
+	
+	public int get_value() {
+		return value;
+	}// end get value
+	
+	public void is_settable(boolean val) {
+		settable = val;
+	}// end is_settable
+} 
