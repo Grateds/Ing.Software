4c3322d40cef75d0bbe2f472691998d5b2534765
vercryger <abuzaid.karim@gmail.com>
2013-10-08 14:20:45 -0300

    pre merge Cell.java

--- src/main/java/com/Grateds/Reversi/MODEL/Cell.java
+++ src/main/java/com/Grateds/Reversi/MODEL/Cell.java
@@ -5,20 +5,26 @@ public class Cell implements Model {
 	private int value; // value of the cell
 	private boolean settable; // field that indicates if cell is settable
 
+	public Cell() {
+		value = 0;
+		settable = true; // default
+	} // end constructor
+	
 	public void initialization() {
 		value = 0;
 		settable = true; // default
-	} // end initialization
+	}
 	
 	public void set_value(int new_value) {
 		value = new_value;
+		settable=false;
 	} // end set_value
 	
 	public int get_value() {
 		return value;
 	}// end get value
 	
-	public void is_settable(boolean val) {
-		settable = val;
+	public boolean is_settable() {
+		return settable;
 	}// end is_settable
 }
