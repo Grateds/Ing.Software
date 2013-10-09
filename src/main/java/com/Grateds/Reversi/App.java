d65d67e45f47021d38a11608a3b85101d7692228
vercryger <abuzaid.karim@gmail.com>
2013-10-09 00:16:42 -0300

    Example for Test

--- src/main/java/com/Grateds/Reversi/App.java
+++ src/main/java/com/Grateds/Reversi/App.java
@@ -5,5 +5,8 @@ import com.Grateds.Reversi.CONTROLLER.*;
 public class App {
     public static void main( String[] args ){
     	 System.out.println( "It's OK" );
+    	 User u = new User();
+    	 u.create("Antoni45", 456123);
+    	 u.create("El_pabo", 45123749);
     }
 }
