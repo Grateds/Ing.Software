5a8af3d0ac2e306f32da4ab93eb3f2b627d3c763
vercryger <abuzaid.karim@gmail.com>
2013-10-09 00:17:21 -0300

    Class User

--- src/main/java/com/Grateds/Reversi/User.java
+++ src/main/java/com/Grateds/Reversi/User.java
@@ -3,7 +3,6 @@ package com.Grateds.Reversi;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 import java.util.UUID;
 import java.util.Vector;
 
