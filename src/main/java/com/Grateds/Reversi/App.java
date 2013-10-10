a4c09b35c9f1c62da5c6158188d39005e664e047
vercryger <abuzaid.karim@gmail.com>
2013-10-09 21:16:21 -0300

    Using controller example

--- src/main/java/com/Grateds/Reversi/App.java
+++ src/main/java/com/Grateds/Reversi/App.java
@@ -9,6 +9,9 @@ public class App {
     	 u.create("Antoni45", 456123);
     	 u.create("El_pabo", 45123749);
     	 Controller controller = new Controller();
-    	 controller.start_game();		
+    	 controller.start_game();	
+    	 controller.set_piece(1, 1, "WHITE");
+    	 System.out.println("Score BLACK PIECES = "+controller.get_score().elementAt(0));
+    	 System.out.println("Score WHITE PIECES = "+controller.get_score().elementAt(1));
     }
 }
