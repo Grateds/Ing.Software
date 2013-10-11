420725d74fc87ac83fd0a2b2de845f7a676c2448
vercryger <abuzaid.karim@gmail.com>
2013-10-11 18:31:14 -0300

    Example for Test User

--- src/main/java/com/Grateds/Reversi/App.java
+++ src/main/java/com/Grateds/Reversi/App.java
@@ -3,15 +3,17 @@ package com.Grateds.Reversi;
 import com.Grateds.Reversi.CONTROLLER.*;
 
 public class App {
+
     public static void main( String[] args ){
     	 System.out.println( "It's OK" );
     	 User u = new User();
-    	 u.create("Antoni45", 456123);
-    	 u.create("El_pabo", 45123749);
+    	 u.delete(2122286520);
     	 Controller controller = new Controller();
     	 controller.start_game();	
     	 controller.set_piece(1, 1, "WHITE");
     	 System.out.println("Score BLACK PIECES = "+controller.getBlackScore());
-    	 System.out.println("Score WHITE PIECES = "+controller.getWhiteScore());
+    	 System.out.println("Score WHITE PIECES = "+controller.getWhiteScore());    	 
     }
 }
+    
+    
