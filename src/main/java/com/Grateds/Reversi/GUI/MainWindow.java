0999b8cd829a0fad3643be378b62599c1da48bdd
Dario <astorgadm@gmail.com>
2013-10-30 11:54:36 -0200

    Update MainWindow.java

--- src/main/java/com/Grateds/Reversi/GUI/MainWindow.java
+++ src/main/java/com/Grateds/Reversi/GUI/MainWindow.java
@@ -53,6 +53,7 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
 
@@ -85,10 +86,17 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
             }
         });
 
-        jButton3.setText("Change Piece");
+        jButton3.setText("First Human");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
+            }
+        });
+        
+        jButton4.setText("Change Piece");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
             }
         });
 
@@ -114,6 +122,7 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
                     .addComponent(jButton1)
                     .addComponent(jButton2)
                     .addComponent(jButton3)
+                    .addComponent(jButton4)
                     .addComponent(jLabel1)
                     .addComponent(jLabel2))
                 .addContainerGap(74, Short.MAX_VALUE))
@@ -132,6 +141,8 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
                         .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton4)
                         .addGap(18, 18, 18)
                         .addComponent(jLabel1)
                         .addGap(18, 18, 18)
@@ -159,23 +170,29 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-    	controller.reset_game_first_computer();
-    	System.out.print("Comienza jugando la computadora");
+    	controller.reset_game_first(false);
+    	System.out.print("Comienza jugando la computadora.!");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+    	controller.stop();
+   	 	controller.reset_game_first(true);
+   	 	System.out.print("Comienza jugando el humano.!");  
     }//GEN-LAST:event_jButton3ActionPerformed
     
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    	// TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     public void update(Observable o, Object arg) {
-    	    setScores(controller.getBlackScore(),controller.getWhiteScore());
-    	    jLabel1.setText("Black: "+BlackScore);  // score of Black piece
-    	    jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 15)); 
-    	    jLabel1.setForeground(new java.awt.Color(0, 18, 18));        
-    	    jLabel2.setText("White: "+WhiteScore);  // score of White piece
-    	    jLabel2.setFont(new java.awt.Font("Ubuntu", 1, 15)); 
-    	    jLabel2.setForeground(new java.awt.Color(10, 20, 0));        
-    	    this.repaint();
+    	setScores(controller.getBlackScore(),controller.getWhiteScore());
+    	jLabel1.setText("Black: "+BlackScore);  // score of Black piece
+    	jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 15)); 
+    	jLabel1.setForeground(new java.awt.Color(0, 18, 18));        
+    	jLabel2.setText("White: "+WhiteScore);  // score of White piece
+    	jLabel2.setFont(new java.awt.Font("Ubuntu", 1, 15)); 
+    	jLabel2.setForeground(new java.awt.Color(10, 20, 0));        
+    	this.repaint();
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -184,6 +201,7 @@ public class MainWindow extends javax.swing.JFrame implements Observer {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
